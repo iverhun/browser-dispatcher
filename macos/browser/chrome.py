76223187726d94d572d3d01f85b431e8d4e74378
Ihor Banadiga <banadiga@gmail.com>
2020-02-09 16:45:39 +0200

    chrome guest support added

--- macos/browser/chrome.py
+++ macos/browser/chrome.py
@@ -1,15 +1,24 @@
 
+
 class Chrome:
     def __init__(self, target=None, url=None):
         self.target = target
         self.url = url
 
     def command(self):
-        return (self._open_incognito if self.target.incognito else self._open_in_profile)()
+        if self.target.guest:
+            return self._open_guest()
+        if self.target.incognito:
+            return self._open_incognito()
+        return self._open_in_profile()
 
     def _open_in_profile(self):
         return '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --profile-directory="{}" "{}" >> /dev/null &' \
             .format(self.target.profile, self.url)
+
+    def _open_guest(self):
+        return '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --guest "{}" >> /dev/null &'\
+            .format(self.url)
 
     def _open_incognito(self):
         return '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --profile-directory="{}" --incognito "{}" >> /dev/null &'\
